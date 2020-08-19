3e45e670f3161db30651b91833a94665e1411eb1
Simone Bacchio <s.bacchio@gmail.com>
2020-08-19 18:06:49 +0300

    Updating all workflows

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     entry_points={
         "console_scripts": ["lyncs_setuptools = lyncs_setuptools:print_keys"]
     },
-    install_requires=["gitpython",],
+    install_requires=["gitpython", "cmake"],
     data_files=[("test", ["test/CMakeLists.txt"])],
     keywords=["Lyncs", "setuptools", "cmake",],
+    extras_require={"test": ["pytest", "pytest-cov",]},
 )
