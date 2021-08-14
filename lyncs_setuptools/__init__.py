78bafb5dbff7898ab5e4932a2008bcd3e4eb223b
Simone Bacchio <s.bacchio@gmail.com>
2021-08-14 17:59:34 +0300

    Protecting import of cmake and pylint

--- lyncs_setuptools/__init__.py
+++ lyncs_setuptools/__init__.py
@@ -9,5 +9,18 @@ from .data_files import *
 from .description import *
 from .classifiers import *
 from .author import *
-from .cmake import *
 from .setuptools import *
+
+try:
+    from .cmake import *
+
+    WITH_CMAKE = True
+except ImportError:
+    WITH_CMAKE = False
+
+try:
+    from .pylint import *
+
+    WITH_PYLINT = True
+except ImportError:
+    WITH_PYLINT = False
